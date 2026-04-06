85ea099b96bf4845a543c4e1657023ee49bb9ef2
HaikalRabbani <muhaikalrabbani@gmail.com>
2026-04-06 13:05:09 +0700

    feat: update catch-all route to improve URL handling for Vue Router

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Catch-all route: Semua URL bakalan diarahkan ke welcome.blade.php
+// Nanti Vue Router yang ambil alih buat nampilin halamannya
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*');
